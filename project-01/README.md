f58b768227be97d2ee51e56192365d6c82ea0c35
Anmol Shah <anmolsha80@gmail.com>
2025-12-25 23:39:31 +0545

    main - Generate realistic test data in JSON from Mockaroo - Create /api/users REST API endpoint to fetch the JSON data from data/MOCK_DATA.json - Create /users endpoint to send the HTML for users data mapped in ul and li elements

---
+++ project-01/README.md
@@ -0,0 +1,7 @@
+## Create REST API
+
+- Generate realistic test data in JSON from [Mockaroo](https://www.mockaroo.com/)
+
+- Create `/users` endpoint to send the HTML for users data mapped in `ul` and `li` elements
+
+- Create `/api/users` REST API endpoint to fetch the JSON data from `data/MOCK_DATA.json`
---
+++ project-01/index.js
@@ -0,0 +1,39 @@
+const express = require('express');
+
+const users = require('./data/MOCK_DATA.json');
+
+const app = express();
+
+const PORT = process.env.PORT || 8000;
+
+app.get('/', (req, res) => {
+  res.send('Welcome to the Home Page!');
+});
+
+app.get('/users', (req, res) => {
+  const html = `
+    <p style="font-weight: bold; margin-bottom: 1.75rem; margin-left: 1rem;">List of users (${
+      users.length
+    }):</p>
+
+    <ul style="display: flex; flex-direction: column; gap: 1rem;">
+      ${users
+        .map(
+          (user) =>
+            `<li>${user.first_name} ${user.last_name} - ${user.email}</li>`,
+        )
+        .join('')}
+    </ul>
+  `;
+
+  return res.send(html);
+});
+
+// REST API
+app.get('/api/users', (req, res) => {
+  return res.json(users);
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
