0d328d15e18a84c9fffb0f5e0990478122a11f90
Anmol Shah <anmolsha80@gmail.com>
2026-01-03 15:17:18 +0545

    main - Create GET, POST, PATCH, and DELETE routes to fetch existing users, create a new user, update existing user, and delete existing user respectively. - Add uuid npm package to generate a string of random characters for user ID - Add nodemon npm package as a dev dependency to automatically restart dev server when a file change happens

--- project-01/index.js
+++ project-01/index.js
@@ -1,10 +1,17 @@
 const express = require('express');
+const { v4: uuidv4 } = require('uuid');
+const fs = require('fs');
 
-const users = require('./data/MOCK_DATA.json');
+const MOCK_DATA_FILE_PATH = './data/MOCK_DATA.json';
+
+let users = require(MOCK_DATA_FILE_PATH);
 
 const app = express();
 
 const PORT = process.env.PORT || 8000;
+
+// Middleware
+app.use(express.urlencoded({ extended: false }));
 
 app.get('/', (req, res) => {
   res.send('Welcome to the Home Page!');
@@ -20,7 +27,7 @@ app.get('/users', (req, res) => {
       ${users
         .map(
           (user) =>
-            `<li>${user.first_name} ${user.last_name} - ${user.email}</li>`,
+            `<li>${user.first_name} ${user.last_name} (${user.gender}) - ${user.email}</li>`,
         )
         .join('')}
     </ul>
@@ -34,16 +41,108 @@ app.get('/api/users', (req, res) => {
   return res.json(users);
 });
 
-app.get('/api/users/:id', (req, res) => {
-  const { id } = req.params;
+// Route grouping
+app
+  .route('/api/users/:id')
+  .get((req, res) => {
+    const { id } = req.params;
 
-  const user = users.find((user) => user.id === parseInt(id));
+    const user = users.find((user) => user.id === id);
 
-  if (!user) {
-    return res.status(404).json({ message: 'User not found' });
-  } else {
-    return res.json(user);
-  }
+    if (!user) {
+      return res.status(404).json({ message: 'User not found' });
+    } else {
+      return res.json(user);
+    }
+  })
+  .patch((req, res) => {
+    const { id } = req.params;
+    const body = req.body;
+
+    const user = users.find((user) => user.id === id);
+
+    if (!user) {
+      return res.status(404).json({ message: 'User not found' });
+    }
+
+    users = users.map((user) => {
+      if (user.id === id) {
+        return {
+          ...user,
+          ...body,
+        };
+      }
+
+      return user;
+    });
+
+    fs.writeFile(MOCK_DATA_FILE_PATH, JSON.stringify(users), (error) => {
+      if (error) {
+        console.log('Patch req error: ', error);
+
+        return res.status(500).send({
+          message: `An error occurred while updating the user with ID: ${id}`,
+        });
+      }
+
+      const updatedUser = users.find((user) => user.id === id);
+
+      return res.send(updatedUser);
+    });
+  })
+  .delete((req, res) => {
+    const { id } = req.params;
+
+    const user = users.find((user) => user.id === id);
+
+    if (!user) {
+      return res.status(404).send({ message: 'User not found' });
+    }
+
+    users = users.filter((user) => user.id !== id);
+
+    fs.writeFile(MOCK_DATA_FILE_PATH, JSON.stringify(users), (error) => {
+      if (error) {
+        console.log('Delete req error: ', error);
+
+        return res.status(500).send({
+          message: `An error occurred while deleting the user with ID: ${id}`,
+        });
+      }
+
+      return res.send(users);
+    });
+  });
+
+app.post('/api/users', (req, res) => {
+  const id = uuidv4();
+
+  const body = req.body;
+
+  const newUser = {
+    id,
+    first_name: body.first_name,
+    last_name: body.last_name,
+    email: body.email,
+    gender: body.gender,
+    job_title: body.job_title,
+  };
+
+  users.push(newUser);
+
+  fs.writeFile(MOCK_DATA_FILE_PATH, JSON.stringify(users), (error) => {
+    if (error) {
+      console.log('Post req error: ', error);
+
+      return res
+        .status(500)
+        .send({ message: 'An error occurred while creating a new user' });
+    }
+
+    const createdUser = users.find((user) => user.id === id);
+
+    return res.send(createdUser);
+  });
 });
 
 app.listen(PORT, () => {
