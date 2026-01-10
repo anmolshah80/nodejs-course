e0e11680bd5a6c26b7b90ddab2d413dd04f38d73
Anmol Shah <anmolsha80@gmail.com>
2026-01-10 13:44:05 +0545

    projects/create-short-url-project - Create routes, controllers, and models to segregate the short-url node application to use the model-view-controller pattern - Create url schema to store shortId, redirect url and, visit history details in the database - Create handleGenerateNewShortURL controller function to create a shortId for the input redirect url - Create handleGetAnalytics controller function to return the total clicks and visit timestamp details for a particular shortId

---
+++ short-url/controllers/url.js
@@ -0,0 +1,42 @@
+const { nanoid } = require('nanoid');
+
+const URL = require('../models/url');
+
+const { SHORT_ID_MAX_LENGTH } = require('../lib/constants');
+
+async function handleGenerateNewShortURL(req, res, next) {
+  const body = req.body;
+
+  if (!body.url) {
+    return res
+      .status(400)
+      .json({ status: 'failed', error: 'url field is required' });
+  }
+
+  const shortID = nanoid(SHORT_ID_MAX_LENGTH);
+
+  await URL.create({
+    shortId: shortID,
+    redirectURL: body.url,
+    visitHistory: [],
+  });
+
+  return res.status(201).json({ status: 'success', shortId: shortID });
+}
+
+async function handleGetAnalytics(req, res, next) {
+  const { shortId } = req.params;
+
+  const result = await URL.findOne({ shortId });
+
+  return res.json({
+    status: 'success',
+    totalClicks: result.visitHistory.length,
+    analytics: result.visitHistory,
+  });
+}
+
+module.exports = {
+  handleGenerateNewShortURL,
+  handleGetAnalytics,
+};
---
+++ short-url/lib/constants.js
@@ -0,0 +1,5 @@
+const SHORT_ID_MAX_LENGTH = 8;
+
+module.exports = {
+  SHORT_ID_MAX_LENGTH,
+};
