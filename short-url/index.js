e1dd1ba6f3c395a3aafe6312d4f3cc23547efdaf
Anmol Shah <anmolsha80@gmail.com>
2026-01-10 13:44:05 +0545

    projects/create-short-url-project - Create routes, controllers, and models to segregate the short-url node application to use the model-view-controller pattern - Create url schema to store shortId, redirect url and, visit history details in the database - Create handleGenerateNewShortURL controller function to create a shortId for the input redirect url - Create handleGetAnalytics controller function to return the total clicks and visit timestamp details for a particular shortId

---
+++ short-url/index.js
@@ -0,0 +1,46 @@
+const express = require('express');
+
+const URL = require('./models/url');
+
+const { connectToMongoDB } = require('./connect');
+
+const urlRoute = require('./routes/url');
+
+const app = express();
+
+const PORT = process.env.PORT || 8001;
+
+connectToMongoDB('mongodb://127.0.0.1:27017/short-url')
+  .then(() => console.log('MongoDb connected!'))
+  .catch((err) => console.log('MongoDb connection error: ', err));
+
+// app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+app.use('/url', urlRoute);
+
+app.get('/:shortId', async (req, res) => {
+  const { shortId } = req.params;
+
+  if (!shortId)
+    return res
+      .status(400)
+      .json({ error: 'a valid shortId needs to be provided' });
+
+  const entry = await URL.findOneAndUpdate(
+    { shortId },
+    {
+      $push: {
+        visitHistory: {
+          timestamp: Date.now(),
+        },
+      },
+    },
+  );
+
+  res.redirect(entry.redirectURL);
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
