8de7705af99e50642fb69bffd14ef263293770df
Anmol Shah <anmolsha80@gmail.com>
2026-02-07 20:03:31 +0545

    features/create-user-model-for-short-url-project - Fix the zod schema for User model to validate the sign-up form - Add an example ZOD_ERRORS array in constants.js - Fix the layout of sign-up form

--- short-url/controllers/user.js
+++ short-url/controllers/user.js
@@ -1,25 +1,38 @@
-const { v4: uuidv4 } = require('uuid');
+const { v4: uuidv4 } = require("uuid");
+const z = require("zod");
 
-const User = require('../models/user');
-const { setUser } = require('../services/auth');
+const User = require("../models/user");
+const { setUser } = require("../services/auth");
+const { UserSchema } = require("../lib/schema");
 
 async function handleUserSignup(req, res, next) {
-  const { name, email, password } = req.body;
+  const { fullName, email, password, confirmPassword } = req.body;
 
-  if (!name || !email || !password) {
-    return res.status(400).json({
-      status: 'failed',
-      error: 'name, email, and password are required',
+  try {
+    console.log("req.body: ", req.body);
+
+    UserSchema.parse({ fullName, email, password, confirmPassword });
+
+    // before creating the user, encrypt the password using bcrypt library
+
+    await User.create({
+      name: fullName,
+      email,
+      password,
     });
+
+    return res.status(201).redirect("/");
+  } catch (error) {
+    console.log("error: ", error);
+
+    if (error instanceof z.ZodError) {
+      return res.status(400).render("signup", {
+        zodErrors: JSON.parse(error),
+      });
+    }
+
+    return res.status(500).send("Internal Server Error");
   }
-
-  await User.create({
-    name,
-    email,
-    password,
-  });
-
-  return res.status(201).redirect('/');
 }
 
 async function handleUserLogin(req, res, next) {
@@ -28,8 +41,8 @@ async function handleUserLogin(req, res, next) {
   const user = await User.findOne({ email, password });
 
   if (!user) {
-    return res.render('login', {
-      error: 'Invalid email or password',
+    return res.render("login", {
+      error: "Invalid email or password",
     });
   }
 
@@ -37,9 +50,9 @@ async function handleUserLogin(req, res, next) {
 
   setUser(sessionId, user);
 
-  res.cookie('short-url-uid', sessionId);
+  res.cookie("short-url-uid", sessionId);
 
-  return res.redirect('/');
+  return res.redirect("/");
 }
 
 module.exports = {
--- short-url/lib/constants.js
+++ short-url/lib/constants.js
@@ -1,5 +1,59 @@
 const SHORT_ID_MAX_LENGTH = 8;
 
+const ZOD_ERRORS = [
+  {
+    origin: "string",
+    code: "too_small",
+    minimum: 1,
+    inclusive: true,
+    path: ["fullName"],
+    message: "Please enter your full name",
+  },
+  {
+    origin: "string",
+    code: "invalid_format",
+    format: "email",
+    pattern:
+      "/^(?!\\.)(?!.*\\.\\.)([A-Za-z0-9_'+\\-\\.]*)[A-Za-z0-9_+-]@([A-Za-z0-9][A-Za-z0-9\\-]*\\.)+[A-Za-z]{2,}$/",
+    path: ["email"],
+    message: "Please enter a valid email address",
+  },
+  {
+    origin: "string",
+    code: "too_small",
+    minimum: 8,
+    inclusive: true,
+    path: ["password"],
+    message: "Password should at least be 8 characters long",
+  },
+  {
+    code: "custom",
+    path: ["password"],
+    message: "Password should have at least one uppercase character",
+  },
+  {
+    code: "custom",
+    path: ["password"],
+    message: "Password should have at least one lowercase character",
+  },
+  {
+    code: "custom",
+    path: ["password"],
+    message: "Password should have at least one number",
+  },
+  {
+    code: "custom",
+    path: ["password"],
+    message: "Password should have at least one special character (!@#$%^&*)",
+  },
+  {
+    code: "custom",
+    path: ["confirmPassword"],
+    message: "Password and confirm password do not match",
+  },
+];
+
 module.exports = {
   SHORT_ID_MAX_LENGTH,
+  ZOD_ERRORS,
 };
