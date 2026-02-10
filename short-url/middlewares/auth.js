fdadd8393b170789f8a1fc3b192130d72c95c247
Anmol Shah <anmolsha80@gmail.com>
2026-02-11 01:45:14 +0545

    features/add-jwt-authentication - Create a forbidden.ejs file to render an HTML template for unauthorized routes - Create checkForAuthentication middleware function to verify whether the user is logged-in before accessing a protected route - Create restrictTo middleware function to restrict users with specific roles access a forbidden route

--- short-url/middlewares/auth.js
+++ short-url/middlewares/auth.js
@@ -1,13 +1,58 @@
 const { getUser } = require("../services/auth");
 
-async function restrictToLoggedInUserOnly(req, res, next) {
+function checkForAuthentication(req, res, next) {
+  // console.log("req.headers: ", req.headers);
+
+  // const authorizationHeaderValue = req.headers["authorization"];
+
+  // req.user = null;
+
+  // if (
+  //   !authorizationHeaderValue ||
+  //   !authorizationHeaderValue.startsWith("Bearer")
+  // )
+  //   return next();
+
+  // const token = authorizationHeaderValue.split("Bearer ")[1];
+
+  // const user = getUser(token);
+
+  // req.user = user;
+
+  // return next();
+
   const token = req.cookies["short-url-jwt"];
 
-  if (!token) return res.redirect("/login");
+  if (!token) return next();
 
   const user = getUser(token);
 
-  if (!user) return res.redirect("/login");
+  if (!user) return res.status(401).redirect("/login");
+
+  req.user = user;
+
+  next();
+}
+
+function restrictTo(roles = []) {
+  return function (req, res, next) {
+    if (!req.user) return res.status(401).redirect("/login");
+
+    if (!roles.includes(req.user.role))
+      return res.status(403).render("forbidden");
+
+    return next();
+  };
+}
+
+async function restrictToLoggedInUserOnly(req, res, next) {
+  const token = req.cookies["short-url-jwt"];
+
+  if (!token) return res.status(401).redirect("/login");
+
+  const user = getUser(token);
+
+  if (!user) return res.status(401).redirect("/login");
 
   req.user = user;
 
@@ -27,4 +72,6 @@ async function checkAuth(req, res, next) {
 module.exports = {
   restrictToLoggedInUserOnly,
   checkAuth,
+  checkForAuthentication,
+  restrictTo,
 };
