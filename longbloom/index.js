6aeb47eef550b1b06edc005c714bdde862dca1b4
Anmol Shah <anmolsha80@gmail.com>
2026-02-12 23:56:08 +0545

    projects/create-blogs-application - Install mongoose to define model schema and zod for form validation - Create lib/schema.js file to define rules for zod validation - Use Node.js' crypto library to hash password before storing it in the database - Create signup.ejs file to register new users

--- longbloom/index.js
+++ longbloom/index.js
@@ -1,15 +1,26 @@
 const path = require("path");
 const express = require("express");
+const mongoose = require("mongoose");
+
+const userRoute = require("./routes/user");
 
 const app = express();
 
 const PORT = 8000;
 
+mongoose
+  .connect("mongodb://localhost:27017/longbloom")
+  .then(() => console.log("MongoDb connected!"))
+  .catch((error) => console.log("Error connecting to MongoDb: ", error));
+
 app.set("view engine", "ejs");
 app.set("views", path.resolve("./views"));
 
+app.use(express.urlencoded({ extended: false }));
 // Source - https://stackoverflow.com/a/32303676
 app.use(express.static(path.join(__dirname, "public")));
+
+app.use("/user", userRoute);
 
 app.get("/", (req, res, next) => {
   res.render("home");
---
+++ longbloom/lib/schema.js
@@ -0,0 +1,48 @@
+const z = require("zod");
+
+// Source -> https://github.com/colinhacks/zod/discussions/3412#discussioncomment-9916377
+const passwordSchema = z
+  .string()
+  .min(8, { error: "Password should at least be 8 characters long" })
+  .max(24, { error: "Password cannot be more than 24 characters" })
+  .refine((password) => /[A-Z]/.test(password), {
+    error: "Password should have at least one uppercase character",
+  })
+  .refine((password) => /[a-z]/.test(password), {
+    error: "Password should have at least one lowercase character",
+  })
+  .refine((password) => /[0-9]/.test(password), {
+    error: "Password should have at least one number",
+  })
+  .refine((password) => /[!@#$%^&*]/.test(password), {
+    error: "Password should have at least one special character (!@#$%^&*)",
+  });
+
+const RegisterFormSchema = z
+  .object({
+    fullName: z
+      .string({ error: "Full name is required" })
+      .min(1, { error: "Please enter your full name" })
+      .max(70, {
+        error: "Name cannot be more than 70 characters",
+      }),
+    email: z.email({ error: "Please enter a valid email address" }),
+    password: passwordSchema,
+    confirmPassword: z
+      .string({ error: "Confirm password is required" })
+      .min(1, { error: "Confirm password cannot be empty" }),
+  })
+  .refine((data) => data.password === data.confirmPassword, {
+    error: "Password and confirm password do not match",
+    path: ["confirmPassword"],
+  });
+
+const LoginFormSchema = z.object({
+  email: z.email({ error: "Please enter a valid email address" }),
+  password: passwordSchema,
+});
+
+module.exports = {
+  RegisterFormSchema,
+  LoginFormSchema,
+};
